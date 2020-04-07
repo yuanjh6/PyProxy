2bc241923872a922b3562fea81cae6a72197c39c
hello886 <553882335@163.com>
2020-04-07 16:48:29 +0800

    临时保存

--- Proxy.py
+++ Proxy.py
@@ -18,32 +18,39 @@ class Proxy(object):
     proxyPattern01 = "<td[^>]*>((\d+)\.(\d+).(\d+).(\d+))</td>[\w\W]*?<td[^>]*>(\d+)</td>[\w\W]*?<td[^>]*>(HTTPS?)</td>"
     proxyPattern02 = "<td>((\d+)\.(\d+).(\d+).(\d+)):(\d+)</td>[\w\W]*?<td>(HTTPS?).*?</td>"
     proxyUrls = ['https://www.xicidaili.com/nn',
-                 # 'https://www.xicidaili.com/nt',
-                 # 'https://www.xicidaili.com/wn',
-                 # 'https://www.xicidaili.com/wt',
-                 # 'https://proxy.mimvp.com/freeopen.php',
-                 # 'http://www.nimadaili.com/https/',
-                 # 'https://www.kuaidaili.com/free/intr/',
-                 # 'http://www.nimadaili.com/http/',
-                 # 'http://www.xiladaili.com/http/',
-                 # 'http://www.superfastip.com/welcome/freeIP',
-                 # 'http://www.xiladaili.com/gaoni/',
-                 # 'http://www.qydaili.com/free/',
-                 # 'https://www.kuaidaili.com/free/inha/',
-                 # 'http://www.nimadaili.com/gaoni/',
-                 # 'http://www.kxdaili.com/dailiip.html',
-                 # 'https://www.kuaidaili.com/ops/',
-                 # 'http://www.xiladaili.com/putong/',
+                 'https://www.xicidaili.com/nt',
+                 'https://www.xicidaili.com/wn',
+                 'https://www.xicidaili.com/wt',
+                 'https://proxy.mimvp.com/freeopen.php',
+                 'http://www.nimadaili.com/https/',
+                 'https://www.kuaidaili.com/free/intr/',
+                 'http://www.nimadaili.com/http/',
+                 'http://www.xiladaili.com/http/',
+                 'http://www.superfastip.com/welcome/freeIP',
+                 'http://www.xiladaili.com/gaoni/',
+                 'http://www.qydaili.com/free/',
+                 'https://www.kuaidaili.com/free/inha/',
+                 'http://www.nimadaili.com/gaoni/',
+                 'http://www.kxdaili.com/dailiip.html',
+                 'https://www.kuaidaili.com/ops/',
+                 'http://www.xiladaili.com/putong/',
                  'http://www.xiladaili.com/https/']
     # 验证代理有效性，此网页返回请求ip
     validProxyUrl = 'http://icanhazip.com/'
 
     def __init__(self):
         proxys = list()
-        pass
+        self.localIp = ''
 
     # 采集代理服务器配置，并验证各个服务器是否好使
     def init_proxy(self):
+        try:
+            res1 = requests.get(self.validProxyUrl, timeout=6, headers=self.headers)
+            if res1.ok:
+                self.localIp = res1.text
+        except Exception as e:
+            print(e.args)
+
         self.crawler_proxy()
         self.valid_proxys()
 
@@ -70,7 +77,7 @@ class Proxy(object):
             response = requests.get(url, headers=self.headers, timeout=(3, 7))
             if not response.ok:
                 return list()
-            html = response.content.decode('utf-8')
+            html = response.text
             group01 = re.findall(re.compile(self.proxyPattern01), html)
             group.extend(group01)
             group02 = re.findall(re.compile(self.proxyPattern02), html)
@@ -87,20 +94,32 @@ class Proxy(object):
 
     # 验证服务器
     def valid_proxys(self):
-        results = filter(self.valid_proxy, self.proxys)
-        self.proxys = [i for i in results]
+        pool = Pool(min(1, cpu_count() - 1))
+        # 在各个进程中打开url，并返回结果
+
+        results = pool.map(self.good_proxy, self.proxys)
+        # 关闭进程池，等待工作结束
+        pool.close()
+        pool.join()
+        results = [x for x in results if x]
+        self.proxys = results
         return results
 
-    # 验证服务器
-    def valid_proxy(self, proxy):
+    # 验证服务器,代理有效，返回代理配置，无效，返回None
+    def good_proxy(self, proxy):
+
         try:
-            res1 = requests.get(self.validProxyUrl, proxies={proxy[0].lower() + "://": proxy[1] + ":" + proxy[2]},
-                                timeout=6)
-            if res1.ok and res1.content == proxy[1]:
-                return True
+            res1 = requests.get(self.validProxyUrl, proxies={
+                proxy[0].lower(): proxy[0].lower() + "://" + proxy[1] + ":" + proxy[2]},
+                                timeout=6, headers=self.headers)
+            print(res1.text)
+            if res1.ok and (
+                    res1.text.find(proxy[1]) > -1 or self.localIp != res1.text):
+                print('good proxy:' + str(proxy))
+                return proxy
         except Exception as e:
             print(e.args)
-        return False
+        return None
 
 
 proxy = Proxy()
