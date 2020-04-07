c7db5625ba0aeb7f821bde75a5728416c8caf206
hello886 <553882335@163.com>
2020-04-07 17:06:46 +0800

    能用版

--- Proxy.py
+++ Proxy.py
@@ -1,10 +1,9 @@
 # -*- coding:utf-8 -*-
 #
 import re
-import urllib
 from multiprocessing.dummy import Pool
 from multiprocessing import cpu_count
-
+import random
 import requests
 
 
@@ -40,6 +39,8 @@ class Proxy(object):
 
     def __init__(self):
         proxys = list()
+        # 当前使用代理id，使用一个+1，滚动使用
+        self.proxy_id = 0
 
     # 采集代理服务器配置，并验证各个服务器是否好使
     def init_proxy(self):
@@ -57,7 +58,7 @@ class Proxy(object):
         pool.join()
         results = [y for x in results for y in x]
         results = self.unique(results)
-        self.proxys = results
+        self.proxyUrls = results
         return results
 
     # 网页采集的[(ip,port),,,]列表信息
@@ -76,45 +77,58 @@ class Proxy(object):
             group.extend(group02)
         except Exception as e:
             print(e.args)
-        print('url:' + url + str(group))
-        return [(x[-1], x[0], x[-2]) for x in group]
+        return [(x[-1] + '://' + x[0] + ":" + x[-2]) for x in group]
 
     # 去重
     def unique(self, proxyUrlList):
-        # TODO
-        return proxyUrlList
+        return list(set(proxyUrlList))
 
     # 验证服务器
     def valid_proxys(self):
         pool = Pool(min(1, cpu_count() - 1))
         # 在各个进程中打开url，并返回结果
 
-        results = pool.map(self.good_proxy, self.proxys)
+        results = pool.map(self.good_proxy, self.proxyUrls)
         # 关闭进程池，等待工作结束
         pool.close()
         pool.join()
         results = [x for x in results if x]
-        self.proxys = results
+        self.proxyUrls = results
         return results
 
     # 验证服务器,代理有效，返回代理配置，无效，返回None
-    def good_proxy(self, proxy):
-
+    def good_proxy(self, proxyUrl):
+        protocol = proxyUrl[:proxyUrl.find(":")].lower()
         try:
             res1 = requests.get(self.validProxyUrl, proxies={
-                proxy[0].lower(): proxy[0].lower() + "://" + proxy[1] + ":" + proxy[2]},
-                                timeout=6, headers=self.headers)
-            print(res1.text)
+                protocol: proxyUrl}, timeout=6, headers=self.headers)
             if res1.ok:
                 return proxy
         except Exception as e:
             print(e.args)
         return None
 
+    def bad_proxy(self, proxyUrl):
+        if self.proxyUrls.contains(proxyUrl):
+            self.proxyUrls.remove(proxyUrl)
 
+    def get_proxy(self):
+        self.proxy_id = (self.proxy_id + 1) % len(self.proxyUrls)
+        return self.proxyUrls[self.proxy_id]
+
+    def random_proxy(self):
+        return self.proxyUrls[random.randint(0, len(self.proxyUrls))]
+
+
+# 使用方法1，打印当前可以使用的代理
+# proxy = Proxy()
+# proxy.init_proxy()
+# print(proxy.proxyUrls)
+
+# 使用方法2，程序中使用
 proxy = Proxy()
-# proxy.get_page_proxy('https://www.kuaidaili.com/free/intr/')
-# proxy.crawler_proxy()
-# proxy.valid_proxy(('HTTP', '124.205.155.153', '124'))
-proxy.init_proxy()
-print(proxy.proxys)
+proxy.init_proxy()  # 抓取代理服务器，验证各代理服务器
+print(proxy.proxyUrls)  # 打印输出当前获取的有效服务器
+proxy.get_proxy()  # 循环获取代理，避免只使用一个
+proxy.random_proxy()  # 随机获取代理，避免只使用一个
+proxy.bad_proxy('1.1.1.1:8080')  # 标记失效代理，从代理列表中移除
